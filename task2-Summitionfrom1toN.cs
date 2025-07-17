be830af8e097e2d10b09f0ffac777af242a6aed7
mohamed fayed <memad4858@gmail.com>
2025-07-18 00:45:47 +0300

    Add project files.

---
+++ task2-Summitionfrom1toN.cs
@@ -0,0 +1,32 @@
+﻿namespace testproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task 2: Summation from 1 to N
+
+            Console.WriteLine("Enter a number N to calculate the sum from 1 to N:");
+
+            string? numberInput = Console.ReadLine();
+            if (int.TryParse(numberInput, out int numberAfterParsing))
+            {
+                // Using the formula for the sum of the first N natural numbers
+                int sumUsingFomula = (numberAfterParsing * (numberAfterParsing + 1)) / 2;
+                int sumUsingForLoop = 0;
+
+                // Using a for loop to calculate the sum from 1 to N
+                for (int i = 1; i <= numberAfterParsing; i++)
+                {
+                    sumUsingForLoop += i;
+                }
+                Console.WriteLine($"The sum from 1 to {numberAfterParsing} by loop is: {sumUsingForLoop} and the Time Complexity is O(N)");
+                Console.WriteLine($"The sum from 1 to {numberAfterParsing} by formula is: {sumUsingFomula} and the Time Complexity is O(C)");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+        }
+    }
+}
